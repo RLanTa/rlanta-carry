b3b19954fb7e483f9b09b00688309994a14ecfb9
RLanTa <55257038+RLanTa@users.noreply.github.com>
2021-03-16 12:15:09 +0300

    Add files via upload

---
+++ rlanta-carry/config.lua
@@ -0,0 +1,3 @@
+-- rlanta#8502 --
+
+Config.Tus = 56 --F9 (Değiştirmek için https://gist.github.com/KingCprey/d40f6deb8ac2949d95524448596e2f37)
---
+++ rlanta-carry/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'adamant'
+game 'gta5'
+
+author 'rlanta#8502'
+
+client_scripts {
+    'cl_carry.lua',
+    'config.lua'
+}
+
+server_scripts {
+    'sv_carry.lua',
+    'config.lua',
+}
