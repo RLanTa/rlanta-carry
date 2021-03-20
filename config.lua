d3fd207467c63822f8da0b1f788aefefcc67e5d3
RLanTa <55257038+RLanTa@users.noreply.github.com>
2021-03-20 22:58:00 +0300

    Delete config.lua

--- config.lua
+++
@@ -1,6 +0,0 @@
--- rlanta#8502 --
-
-Config.TusType = 56 --F9 (Değiştirmek için https://gist.github.com/KingCprey/d40f6deb8ac2949d95524448596e2f37)
-Config.Tusla = true --Tuşla taşımayı aktif etmek için
-Config.Komutla = false --Komutla taşımayı aktif etmek için
-Config.Komut = sirtla --Komut
